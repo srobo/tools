81aa6e671385cf0e6c8790acddab6d3952e23cf4
Robert Spanton <rspanton@zepler.net>
2009-07-06 18:27:00 +0000

    tools: Move farnell part information grabbing into separate module.
    git-svn-id: https://www.studentrobotics.org/svn/tools@3098 98f67ba8-b320-0410-bda0-b753e294843c

---
+++ bom/farnell.py
@@ -0,0 +1,114 @@
+"""Routines for scraping data about parts from Farnell"""
+from urllib import urlopen
+import string, sgmllib, sys
+
+class Item(sgmllib.SGMLParser):
+    "Represents a Farnell item"
+
+    def __init__(self, partNumber, verbose=0):
+        "Initialise an object, passing 'verbose' to the superclass."
+
+        sgmllib.SGMLParser.__init__(self, verbose)
+        self.qty_range = []
+        self.cost = []
+        self.inside_td_element = 0
+        self.inside_p_element = 0
+        self.inside_b_element = 0
+        self.last_data = ''
+        self.qty = True
+
+        self.feed(self.__getData(partNumber))
+        self.close()
+
+    def __getData(self, partNumber):
+        page = urlopen('http://xgoat.com/p/farnell/'+str(partNumber)).read()
+
+        start = string.find(page, '<div id="availability">')
+        if start == -1:
+            raise "Part number doesn't exist"
+
+        info = page[start:]
+        end = string.find(info, '</div>')
+        availInfo = info[:end]
+
+        start = string.find(page, '<div id="price">')
+        if start == -1:
+            raise "Part number doesn't exist"
+
+        info = page[start:]
+        end = string.find(info, '</div>')
+        priceInfo = info[:end]
+
+        return availInfo+priceInfo
+
+    def start_td(self, attributes):
+        "Process a table div."
+        self.inside_td_element += 1
+
+    def end_td(self):
+        "Record the end of a table div."
+        self.inside_td_element -= 1
+
+    def start_p(self, attributes):
+        "Process a paragraph."
+        self.inside_p_element += 1
+
+    def end_p(self):
+        "Record the end of a paragraph."
+        self.inside_p_element -= 1
+
+    def start_b(self, attributes):
+        "Process a bold."
+        self.inside_b_element += 1
+
+    def end_b(self):
+        "Record the end of a bold."
+        self.inside_b_element -= 1
+
+    def handle_data(self, data):
+        "Handle the textual 'data'."
+
+        data = data.replace('\n', '').replace(':', '')
+        if data == '':
+            return
+
+        if self.inside_td_element > 0:
+            #	print 'td:"'+data+'"'
+            if self.qty:
+                self.qty_range.append(data)
+                self.qty = False
+            else:
+                self.cost.append(data[2:])
+                self.qty = True
+
+        elif self.inside_b_element > 0:
+            #	print 'b:"'+data+'"'
+            self.last_data = data
+
+        elif self.inside_p_element > 0:
+            #	print 'p:"'+data+'"'
+            if self.last_data == 'Price For':
+                self.price_for = data
+            elif self.last_data == 'Minimum Order Quantity':
+                self.min_order = int(data)
+            elif self.last_data == 'Order Multiple':
+                self.multi = int(data)
+            elif self.last_data == 'Availability':
+                if data.isdigit():
+                    self.avail = int(data)
+                else:
+                    self.avail = str(data)
+
+    def get_info(self):
+        "Return a dict of the info garnered."
+        return dict(qty=self.qty_range, price=self.cost, num_for_price=self.price_for, min_order=self.min_order, multiple=self.multi, number_available=self.avail)
+
+    def print_info(self):
+        "Print a the info garnered in a nice way."
+        print ' Number Available:',self.avail
+        print ' Price For:',self.price_for
+        print ' Minimum Order Quantity:',self.min_order
+        print ' Order Multiple:',self.multi
+        print ' Pricing:'
+        for i in range(0, len(self.qty_range)):
+            print ' ',self.qty_range[i],'  \t',self.cost[i]

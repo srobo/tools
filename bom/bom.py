6b33aa82c3c88623828e1661e8e9d3e44e4e3185
Robert Spanton <rspanton@zepler.net>
2009-07-06 17:38:02 +0000

    tools: Clean up BOM generation code: move all BOM parsing into bom.py
    This patch moves the EAGLE BOM parser into bom.py.  It changes the old
    "EagleParts" class to be "EagleSchem".  The class now takes an EAGLE
    schematic, so that one doesn't have to replicate BOM exporting
    horror in several places.
    
    
    git-svn-id: https://www.studentrobotics.org/svn/tools@3097 98f67ba8-b320-0410-bda0-b753e294843c

--- bom/bom.py
+++ bom/bom.py
@@ -1,8 +1,13 @@
 # Routines for extracting BOMs from schematics
-import subprocess
+import subprocess, tempfile, os, sys
 
 EAGLE = 0
 UNKNOWN = 1
+
+PARTS_DB = os.path.expanduser("~/.sr/tools/bom/sr_component_lib")
+if not os.path.exists( PARTS_DB ):
+    print "Parts DB not found at \"%s\"" % PARTS_DB
+    sys.exit(1)
 
 def file_is_eagle(f):
     """Return true if the file is an EAGLE file"""
@@ -24,16 +29,52 @@ def schem_type(fname):
 
     return UNKNOWN
 
-def export_eagle_bom(fname, out_fname):
-    p = subprocess.Popen( """sr export_eagle_bom "%s" "%s" """ % (fname, out_fname),
-                          shell = True )
-    p.communicate()
-    p.wait()
+class EagleSchem(dict):
+    """Reads in EAGLE file.
+    Parts in file can be accessed like dictionary."""
 
-def export_bom(fname, ofname):
+    def __init__(self, fname):
+        """Load the given file.  Grab the parts list."""
+        self.fname = fname
+        self.__load_bom()
+
+    def __load_bom(self):
+        # Export the EAGLE BOM to a temporary file
+        tmpf = tempfile.mkstemp()
+        os.close(tmpf[0])
+        tmpf = tmpf[1]
+
+        self.__export_bom(tmpf)
+
+        f = open( tmpf, "r" )
+
+        # Skip and check EAGLE header
+        for i in range(8):
+            l = f.readline()
+            # line 5 starts with "EAGLE" in EAGLE parts lists.
+            if i == 4 and l[0:5] != "EAGLE":
+                raise Exception, "Parts list '%s' is not an EAGLE parts list" % self.fname
+    
+        for line in f:
+            fields = line.split()
+
+            value = fields[1].strip().lower() # e.g. sr-r-10k
+            id = fields[0].strip().upper() # e.g. R1
+            
+            self[id] = value
+
+        f.close()
+        os.remove(tmpf)
+
+    def __export_bom(self, out_fname):
+        p = subprocess.Popen( """sr export_eagle_bom "%s" "%s" """ % (self.fname, out_fname),
+                              shell = True )
+        p.communicate()
+        p.wait()
+
+def open_schem(fname):
     s = schem_type(fname)
     if s == EAGLE:
-        export_eagle_bom(fname, ofname)
+        return EagleSchem(fname)
     else:
         raise "We don't yet support exporting BOMs from non-EAGLE things"
-

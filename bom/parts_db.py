af67d124a4032600c101c4ada0d251814baeaf56
Robert Spanton <rspanton@zepler.net>
2009-07-07 00:06:01 +0000

    sr tools: Create a class that represents a part.
    This class will soon receive functions like "stockcheck".
    
    
    
    git-svn-id: https://www.studentrobotics.org/svn/tools@3105 98f67ba8-b320-0410-bda0-b753e294843c

--- bom/parts_db.py
+++ bom/parts_db.py
@@ -2,25 +2,28 @@
 """Student Robotics parts database access library"""
 import csv
 
+class Part(dict):
+    """Represents a part"""
+    def __init__(self, d):
+        """Initialise from a dict from the DB"""
+        c = d["sr-code"].lower()
+
+        for k in d.keys():
+            if isinstance(d[k], str):
+                d[k] = d[k].strip()
+
+        for k,v in d.iteritems():
+            self[k] = v
+
 class Db(dict):
     def __init__(self, fname):
         f = open(fname, "r")
         r = csv.DictReader(f)
 
         for line in r:
+            # Discard commented out lines
             if line["sr-code"].strip()[0] == "#":
                 continue
-            c = line["sr-code"].lower()
-            # Remove the code from the dictionary
-            del line["sr-code"]
 
-            for k in line.keys():
-                if isinstance(line[k], str):
-                    line[k] = line[k].strip()
-
-            self[c] = line
-
-
-        
-
-
+            part = Part(line)
+            self[ part["sr-code"] ] = part

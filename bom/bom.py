bda2c1315a99603bc241a2e91d9014eab7c8e18e
Robert Spanton <rspanton@zepler.net>
2009-07-10 21:50:38 +0000

    tools: Add a "Bom" class.
    This class stores a board's BOM.  This patch also adds a second class
    called "PartGroup", which groups a set of parts together like a line
    of a BOM.
    
    
    git-svn-id: https://www.studentrobotics.org/svn/tools@3112 98f67ba8-b320-0410-bda0-b753e294843c

--- bom/bom.py
+++ bom/bom.py
@@ -6,4 +6,61 @@ if not os.path.exists( PARTS_DB ):
     print "Parts DB not found at \"%s\"" % PARTS_DB
     sys.exit(1)
 
+class PartGroup(list):
+    """A set of parts
+    One might call this a "BOM line" """
+    def __init__(self, part, name, designators = [] ):
+        list.__init__(self)
+
+        for x in designators:
+            self.append( (name, designators) )
+
+        self.part = part
+        self.name = name
+
+    def stockcheck(self):
+        """Check the distributor has enough parts in stock."""
+        s = self.part.stockcheck()
+        if s == None:
+            return None
+
+        if s < len(self):
+            return False
+        return True
+
+class Bom(dict):
+    """BOM object.
+    Groups parts with the same srcode into PartGroups.
+    Dictionary keys are sr codes."""
+    def __init__(self, db, fname, name ):
+        """fname is the schematic to load from.  
+        db is the parts database object.
+        name is the name to give the schematic."""
+        dict.__init__(self)
+        self.db = db
+        self.name = name
+
+        s = schem.open_schem(fname)
+
+        for des,srcode in s.iteritems():
+            if not self.has_key(srcode):
+                self[srcode] = PartGroup( db[srcode], name )
+            self[srcode].append((name,des))
+
+    def stockcheck(self):
+        """Check that all items in the schematic are in stock.
+        Returns list of things that aren't in stock."""
+        # Things that aren't in stock
+        ns = []
+        # Things that can't be determined:
+        unk = []
+        for pg in self.values():
+            a = pg.stockcheck()
+
+            if a == None:
+                unk.append(pg.part)
+            elif not a:
+                ns.append(pg.part)
+
+        return (ns,unk)
 

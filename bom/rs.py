86c90fa88eaf07fe5221494f4e15840e8b79a9da
Peter Law <PeterJCLaw@gmail.com>
2010-03-25 02:37:13 +0000

    Make the RS script work. RS provides less info that farnell, and in a less useful manner. remove attributes that RS hasn't got.

--- bom/rs.py
+++ bom/rs.py
@@ -40,13 +40,10 @@ class Item(sgmllib.SGMLParser):
         self.qty_range = []
         self.cost = []
         self.inside_td_element = 0
-        self.inside_p_element = 0
-        self.inside_b_element = 0
-        self.last_data = ''
+        self.inside_th_element = 0
         self.qty = True
         self.qty_str = ""
 
-        self.last_qty = None
         self.prices = []
 
         self.feed(self.__getData(partNumber))
@@ -55,23 +52,20 @@ class Item(sgmllib.SGMLParser):
     def __getData(self, partNumber):
         page = grab_url_cached( 'http://xgoat.com/p/rs/'+str(partNumber) )
 
-        start = string.find(page, '<div class="availability">')
+        if string.find(page, 'id="stockY"') == -1:
+            self.avail = 'Out of stock'
+        else:
+            self.avail = 'In stock'
+
+        start = string.find(page, '<table id="productdatatable"')
         if start == -1:
             raise Exception( """Part number "%s" doesn't exist""" % str(partNumber) )
 
         info = page[start:]
-        end = string.find(info, '</div>')
-        availInfo = info[:end]
-
-        start = string.find(page, '<div class="price">')
-        if start == -1:
-            raise Exception( """Part number "%s" doesn't exist""" % str(partNumber) )
-
-        info = page[start:]
-        end = string.find(info, '</div>')
+        end = string.find(info, '</table>')
         priceInfo = info[:end]
 
-        return availInfo+priceInfo
+        return priceInfo
 
     def start_td(self, attributes):
         "Process a table div."
@@ -81,26 +75,18 @@ class Item(sgmllib.SGMLParser):
         "Record the end of a table div."
         self.inside_td_element -= 1
 
-    def start_p(self, attributes):
-        "Process a paragraph."
-        self.inside_p_element += 1
+    def start_th(self, attributes):
+        "Process a table heading."
+        self.inside_th_element += 1
 
-    def end_p(self):
-        "Record the end of a paragraph."
-        self.inside_p_element -= 1
-
-    def start_b(self, attributes):
-        "Process a bold."
-        self.inside_b_element += 1
-
-    def end_b(self):
-        "Record the end of a bold."
-        self.inside_b_element -= 1
+    def end_th(self):
+        "Record the end of a table heading."
+        self.inside_th_element -= 1
 
     def handle_data(self, data):
         "Handle the textual 'data'."
 
-        data = data.replace('\n', '').replace(':', '')
+        data = data.replace('\n', '').replace('\r', '').replace(':', '').replace('\t', '')
         if data.replace(' ', '') == '':
             return
 
@@ -114,47 +100,15 @@ class Item(sgmllib.SGMLParser):
             else:
                 self.qty_str += data
 
-        elif self.inside_b_element > 0:
-            # print 'b:"'+data+'"'
-            self.last_data = data
+        elif self.inside_th_element > 0 and data[9:13] == 'Each':
+            # print 'th:"'+data+'"'
+            self.multi = self._parse_pack_size(data[9:-1])
 
-        elif self.inside_p_element > 0:
-            # print 'p:"'+data+'"'
-            #kill off the last_data, but store it just in case
-            tmp_last_data = self.last_data
-            self.last_data = ''
-            #test for a match to last_data
-            if tmp_last_data == 'Price For':
-                self.price_for = self._parse_price_for(data)
-            elif tmp_last_data == 'Minimum Order Quantity':
-                self.min_order = int(data)
-            elif tmp_last_data == 'Order Multiple':
-                self.multi = int(data)
-            elif tmp_last_data == 'Availability':
-                if data.isdigit():
-                    self.avail = int(data)
-                else:
-                    self.avail = 0
-            else:	#not this time around
-                self.last_data = tmp_last_data
-
-    def _parse_price_for(self, s):
+    def _parse_pack_size(self, s):
         "Break the 'price for' string up"
-        r = re.compile( "Reel of ([0-9,]+)" )
-        m = r.search( s )
-        if m != None:
-            # Strip commas
-            n = m.group(1).replace(",","")
-            return int(n)
-
-        r = re.compile( "Pack of ([0-9,]+)" )
-        m = r.search( s )
-        if m != None:
-            # Strip commas
-            n = m.group(1).replace(",","")
-            return int(n)
-
-        r = re.compile( "([0-9,]+) Each" )
+        if s == 'Each':
+            return 1
+        r = re.compile( "Each \(In a Pack of ([0-9,]+)\)" )
         m = r.search( s )
         if m != None:
             # Strip commas
@@ -205,17 +159,15 @@ class Item(sgmllib.SGMLParser):
 
     def get_info(self):
         "Return a dict of the info garnered."
-        return dict(qty=self.qty_range, price=self.cost, num_for_price=self.price_for, min_order=self.min_order, multiple=self.multi, number_available=self.avail)
+        return dict(qty=self.qty_range, price=self.cost, multiple=self.multi, availability=self.avail)
 
     def print_info(self):
         "Print a the info garnered in a nice way."
-        print ' Number Available:',self.avail
-        print ' Price For:',self.price_for
-        print ' Minimum Order Quantity:',self.min_order
+        print ' Availability:',self.avail
         print ' Order Multiple:',self.multi
         print ' Pricing:'
 
-        n = self.min_order
+        n = self.multi
         for p in self.prices:
             if n != p[0]:
                 print "\t%i - %i: \t£%s" % (n, p[0], p[1])

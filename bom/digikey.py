149ac33d19d9c51346776572f88a6f176d845bd3
Richard Barlow <richard@richardbarlow.co.uk>
2010-06-28 15:37:13 +0100

    Add frontend script to digikey parts db backend.
    Also add 'print_info' function to digikey Item class that prints out
    all info gathered on an item. Had to add a line to set the file
    encoding too.

--- bom/digikey.py
+++ bom/digikey.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Routines for scraping data about parts from digikey"""
 from cachedfetch import grab_url_cached
 
@@ -10,3 +11,19 @@ class Item:
         self.price_for = 1
         self.multi = 1
         self.prices = []
+
+    def print_info(self):
+        """Print all of the info on the part"""
+        print ' Number Available:',self.avail
+        print ' Price For:',self.price_for
+        print ' Minimum Order Quantity:',self.min_order
+        print ' Order Multiple:',self.multi
+        print ' Pricing:'
+
+        n = self.min_order
+        for p in self.prices:
+            if n != p[0]:
+                print "\t%i - %i: \t£%s" % (n, p[0], p[1])
+                n = p[0] + 1
+            else:
+                print "\t%i +: \t£%s" % (n, p[1])

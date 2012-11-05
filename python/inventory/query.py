8f5967a2bbd8fc3041184db61c5dcad9de524291
Chris Kirkham <chrisjameskirkham@gmail.com>
2012-11-05 23:01:12 +0000

    Wrap search tree execution up in a function returning assets
    query.query(...) is a nice high-level function taking in a query
    string and returning (via a generator) all of the matching parts.

---
+++ python/inventory/query.py
@@ -0,0 +1,17 @@
+import inventory
+import oldinv
+import query_parser
+
+def _get_inv():
+    top = oldinv.gettoplevel()
+    if top is None:
+        print >>sys.stderr, "Error: Must be run from within the inventory."
+        exit(1)
+    return inventory.Inventory(top).root
+
+def query(query_str):
+    inv = _get_inv()
+    tree = query_parser.search_tree(query_str)
+    for part in inv.parts.values():
+        if tree.match(part):
+            yield part

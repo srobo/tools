05ee95df3f2f90776ba7da6e09cc4dd374087cab
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-01-16 19:34:59 +0000

    Further fix up spending.py budget interaction

--- python/spending/spending.py
+++ python/spending/spending.py
@@ -1,5 +1,5 @@
 "Library for accessing the spending files"
-import yaml, os, sys, datetime, sr.budget
+import yaml, os, sys, datetime, sr.budget as budget
 from decimal import Decimal as D
 
 class Transaction(object):

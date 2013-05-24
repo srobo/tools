90cf8da17db3e57511f893a2ca7546b76160e0e9
Rob Spanton <rspanton@zepler.net>
2013-05-24 20:56:39 +0100

    Optionally store one's SR password in a keyring
    If one sets "use_keyring" in the config to True, then the
    get_password() function of a Config object will attempt to lookup the
    user's password in the keyring.
    
    At the time of writing, the keyring library
    ( https://pypi.python.org/pypi/keyring ) supports a variety of
    keyrings from different platforms (Gnome, KDE, OSX, and some kind of
    Windows thing).
    
    After the first password entry, the password is stored in the keyring,
    and the user should never have to enter their password again.

--- python/config.py
+++ python/config.py
@@ -1,7 +1,13 @@
 "Config for the SR tools"
+import getpass
 import os
 from subprocess import check_output
 import yaml
+
+try:
+    import keyring
+except ImportError:
+    keyring = None
 
 class Config(dict):
     "Configuration reader for the SR tools"
@@ -36,5 +42,68 @@ class Config(dict):
             d = yaml.load(f, Loader = yaml.CLoader )
 
         self.update(d)
-        
-        
+
+    def get_user(self, *args):
+        """Get the username
+
+        Return the first non-None argument.
+        If all arguments are None, get the username from the config.
+        If the config doesn't provide the username, prompt the user
+        for it."""
+
+        for arg in args:
+            if arg is not None:
+                return arg
+
+        user = self["user"]
+        if user is not None:
+            return user
+
+        user = raw_input( "SR username: " )
+        return user
+
+    def get_password(self, *args, **kw):
+        """Get the user's password
+
+        Return the first non-None argument.
+        If all of the arguments are None, and the user has enabled the
+        use of the keyring, attempts to look-up password in the
+        keyring.  If that fails, resort to prompting the user for
+        their password.
+
+        For keyring lookups, a username is required.
+        If the caller does not want to use the username specified
+        by the config files for this, then the desired username can be
+        fed in through the 'user' keyword argument.  This argument is
+        ignored if its value is None."""
+
+        for arg in args:
+            if arg is not None:
+                return arg
+
+        if self["use_keyring"]:
+            "Try getting the password from the keyring"
+
+            if "user" in kw and kw["user"] is not None:
+                user = kw["user"]
+            else:
+                user = self["user"]
+
+            if keyring is None:
+                print >>sys.stderr, "Warning: Cannot import keyring module."
+            else:
+                password = keyring.get_password( self["keyring_service"],
+                                                 user )
+                if password is not None:
+                    return password
+
+        # We failed to get the password from the keyring, so prompt the
+        # user for it
+        password = getpass.getpass( "SR password: " )
+
+        if self["use_keyring"] and keyring is not None:
+            "Store password in the keyring"
+            keyring.set_password( self["keyring_service"],
+                                  user, password )
+
+        return password

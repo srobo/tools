2831d3d013ce2da9f6fe1b90206c2eeb71ba5edf
Tom Leese <inbox@tomleese.me.uk>
2014-10-31 10:44:36 +0000

    Add inventory argument to inv-history

--- sr/tools/cli/inventory.py
+++ sr/tools/cli/inventory.py
@@ -9,4 +9,6 @@ def add_subparsers(subparsers):
     parser_history = subparsers.add_parser('inv-history',
                                            help='History about a item.')
     parser_history.add_argument('partcode')
+    parser_history.add_argument('--inventory', '-i', default='.',
+                                help='Location of the inventory.')
     parser_history.set_defaults(func=history)

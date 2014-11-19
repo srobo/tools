7f908ef9c08f4b8c9021e412f00a3c0aa0226bd6
Tom Leese <inbox@tomleese.me.uk>
2014-11-19 22:20:29 +0000

    Start work on inventory history tool

--- sr/tools/cli/inventory.py
+++ sr/tools/cli/inventory.py
@@ -1,8 +1,30 @@
+from __future__ import print_function
+
 import argparse
+import pygit2
+
+import sr.tools.inventory as srinv
+
+
+def walk_history(tree, repo):
+    for entry in tree:
+        yield entry
+
+        o = repo[entry.oid]
+        if isinstance(o, pygit2.Tree):
+            yield from walk_history(o, repo)
 
 
 def history(args):
-    pass
+    partcode = srinv.normalise_partcode(args.partcode)
+    repo = pygit2.Repository(args.inventory)
+    inventory = srinv.Inventory(args.inventory)
+    part = inventory.root.parts[partcode]
+
+    for commit in repo.walk(repo.head.target, pygit2.GIT_SORT_TOPOLOGICAL):
+        for entry in walk_history(commit.tree, repo):
+            if partcode in entry.name:
+                print(entry.name)
 
 
 def add_subparsers(subparsers):

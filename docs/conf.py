e0883aa26c61b589c44604b5a4d4a92a07fdec48
Tom Leese <inbox@tomleese.me.uk>
2014-12-08 12:45:12 +0000

    Add LaTeX preamble to increase TOC depth

--- docs/conf.py
+++ docs/conf.py
@@ -188,14 +188,9 @@ htmlhelp_basename = 'StudentRoboticsToolsdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    'papersize': 'a4paper',
+    'pointsize': '11pt',
+    'preamble': '\setcounter{tocdepth}{2}'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples

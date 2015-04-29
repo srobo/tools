9e542553a0d379385cad3d03e4189bba7943460b
Tom Leese <inbox@tomleese.me.uk>
2015-04-29 14:04:55 +0100

    Fix output of update command

--- sr/tools/cli/update.py
+++ sr/tools/cli/update.py
@@ -3,7 +3,8 @@ from __future__ import print_function
 
 def command(args):
     print('Please update with pip:')
-    print('$ pip install -U sr.tools')
+    print('$ pip install git+git://srobo.org/tools.git')
+    print('Optionally, you can include the `--user` flag.')
 
 
 def add_subparser(subparsers):

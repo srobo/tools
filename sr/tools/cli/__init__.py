782538a23aece5cdd73cfeee0d16d007741fb246
Peter Law <PeterJCLaw@gmail.com>
2023-01-14 12:38:45 +0000

    Make the list-commands test actually work

--- sr/tools/cli/__init__.py
+++ sr/tools/cli/__init__.py
@@ -80,7 +80,7 @@ def main(args=sys.argv):
         name = f'{__name__}.{command}'
         importlib.import_module(name).add_subparser(subparsers)
 
-    args = parser.parse_args()
+    args = parser.parse_args(args=args)
 
     if 'func' in args:
         try:
--- tests/test_command_list.py
+++ tests/test_command_list.py
@@ -7,5 +7,10 @@ import sr.tools.cli
 
 class TestListCommands(unittest.TestCase):
     def test_lists_commands_smoke(self):
-        with contextlib.redirect_stdout(io.StringIO()):
-            sr.tools.cli.main('list-commands')
+        with contextlib.redirect_stdout(io.StringIO()) as buffer:
+            sr.tools.cli.main(['list-commands'])
+
+        self.assertIn(
+            'list-commands',
+            buffer.getvalue().splitlines(),
+        )
